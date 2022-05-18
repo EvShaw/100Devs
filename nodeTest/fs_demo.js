3ec294f075490784effd22cbaabac2cc8053aacb
Evan Shaw <e85.shaw@gmail.com>
2022-05-18 14:45:40 -0700

    folder/ file update

---
+++ nodeTest/fs_demo.js
@@ -0,0 +1,41 @@
+const fs = require('fs')
+const path = require('path')
+
+//create folder: (on system)
+
+// fs.mkdir(path.join(__dirname, '/test'), {}, err => {
+//     if (err) throw err
+//     console.log('foldercreated...')
+// }) //takes in folder I want to create
+
+// //create and write to file
+
+// fs.writeFile(path.join(__dirname, '/test', 'hello.txt'), 'Hello World!', err => {
+//     if (err) throw err
+//     console.log('file written to...')
+
+
+//     //append file - 
+//     fs.appendFile(path.join(__dirname, '/test', 'hello.txt'), 'I love node js', err => {
+//         if (err) throw err
+//         console.log('file written to...')
+//     })
+
+
+
+// })
+
+//read file
+
+// fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', err data) => {
+//     if (err) throw err
+//     console.log(data)
+// }
+
+//Rename a file: 
+
+fs.readFile(path.join(__dirname, '/test', 'hello.txt'), path.join(__dirname, '/test', 'helloworld.txt'), (err) => {
+    if (err) throw err
+    console.log('file renamed...')
+})
+
---
+++ nodeTest/path_demo.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+//Basename - base file name
+
+console.log(path.basename(__filename))
+
+// Directory name
+
+console.log(path.dirname(__filename))
+
+//File extension
+
+console.log(path.extname(__filename))
+
+//Create path object
+
+console.log(path.parse(__filename).base)
+
+//concatenate paths (use join for this)
+../test/hello.html
+console.log(path.join(__dirname, 'test', 'hello.html'))
